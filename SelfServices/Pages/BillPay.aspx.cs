b373ed3cf1727fefd852aca2d7e6416d9c06c690
rajasu8 <sudharsana.lakshmi3@gmail.com>
2015-08-28 11:14:57 +0530

    Modified BillPay

--- SelfServices/Pages/BillPay.aspx.cs
+++ SelfServices/Pages/BillPay.aspx.cs
@@ -15,62 +15,62 @@ namespace SelfServices.Pages
         protected void Page_Load(object sender, EventArgs e)
         {
 
-            ////Session["customerId"] = 208;
-            //customerBill = Bill.GetUserBill((string)Session["customerId"]);
+            //Session["customerId"] = 208;
+            customerBill = Bill.GetUserBill((string)Session["customerId"]);
 
-            //if (customerBill != null)
-            //{
-            //    if (!IsPostBack)
-            //        Session.Add("customerBill", customerBill);
-            //    Bill temp = (Bill)Session["customerBill"];
+            if (customerBill != null)
+            {
+                if (!IsPostBack)
+                    Session.Add("customerBill", customerBill);
+                Bill temp = (Bill)Session["customerBill"];
 
-            //    //Bill temp = Bill.GetUserBill();
-            //    lblPhone.Text = temp.PrimaryPhone;
-            //    lblAccount.Text = temp.AccountNumber;
-            //    lblDate.Text = temp.BillDate;
+                //Bill temp = Bill.GetUserBill();
+                lblPhone.Text = temp.currentBill.PrimaryPhone;
+                lblAccount.Text = temp.currentBill.AccountNumber;
+                lblDate.Text = temp.currentBill.BillDate;
 
-            //    lblPreviousBalance.Text = temp.Account_Summary.Previous_Period.PreviousBalance;
-            //    lblPaymentReceived.Text = temp.Account_Summary.Previous_Period.PaymentReceived;
-            //    lblBalanceForward.Text = temp.Account_Summary.Previous_Period.BalanceForward;
-            //    //foreach (var service in temp.Account_Summary.CurrentCharges.Services)
-            //    //{
-            //    //    ServicesName.Text += service.name+" ";
-            //    //}
-            //    lblFIOS.Text = temp.Account_Summary.CurrentCharges.subtotal;
+                lblPreviousBalance.Text = temp.currentBill.Account_Summary.Previous_Period.PreviousBalance;
+                lblPaymentReceived.Text = temp.currentBill.Account_Summary.Previous_Period.PaymentReceived;
+                lblBalanceForward.Text = temp.currentBill.Account_Summary.Previous_Period.BalanceForward;
+                //foreach (var service in temp.Account_Summary.CurrentCharges.Services)
+                //{
+                //    ServicesName.Text += service.name+" ";
+                //}
+                lblFIOS.Text = temp.currentBill.Account_Summary.Current_Charges.Subtotal.ToString();
 
-            //    //foreach (var service in temp.Account_Summary.AdditionalCharges.AdditionalServices)
-            //    //{
-            //    //    AdditionalServivesName.Text  += service.name + " ";
-            //    //}
+                //foreach (var service in temp.Account_Summary.AdditionalCharges.AdditionalServices)
+                //{
+                //    AdditionalServivesName.Text  += service.name + " ";
+                //}
 
-            //    lblAddService.Text = temp.Account_Summary.AdditionalCharges.subtotal;
-            //    lblFees.Text = temp.taxes;
-            //    lblTotalDue1.Text = temp.totalAmount;
-            //    if(DataAccessHelper.IsBillPaid((string)Session["customerId"],temp.BillDate))
-            //    {
-            //        Pay.Visible = false;
-            //        LabelPay.Visible = true;
-            //        LabelPay.Text = "Thank you for online payment";
-            //    }
-            //    else
-            //    {
-            //        Pay.Visible = true;
-            //    }
+                lblAddService.Text = temp.currentBill.Account_Summary.Additional_Charges.Subtotal;
+                lblFees.Text = temp.currentBill.Taxes;
+                lblTotalDue1.Text = temp.currentBill.Total_Amount;
+                if (DataAccessHelper.IsBillPaid((string)Session["customerId"], temp.currentBill.BillDate))
+                {
+                    Pay.Visible = false;
+                    LabelPay.Visible = true;
+                    LabelPay.Text = "Thank you for online payment";
+                }
+                else
+                {
+                    Pay.Visible = true;
+                }
+            }
         }
-    
 
 
         protected void Pay_Click(object sender, EventArgs e)
         {
-           // ServiceJsonHelper.PayBill((string)Session["customerId"], ((Bill)Session["customerBill"]).totalAmount);
-           // Pay.Visible = false;
-           //LabelPay.Visible = true;
-           //LabelPay.Text = "Thank you for online payment";
+            ServiceJsonHelper.PayBill((string)Session["customerId"], ((Bill)Session["customerBill"]).currentBill.BillDate);
+            Pay.Visible = false;
+            LabelPay.Visible = true;
+            LabelPay.Text = "Thank you for online payment";
         }
 
         protected void LabelPay_Click(object sender, EventArgs e)
         {
-            //Response.Redirect("BillView.aspx");
+            Response.Redirect("BillView.aspx");
         }
     }
 }
